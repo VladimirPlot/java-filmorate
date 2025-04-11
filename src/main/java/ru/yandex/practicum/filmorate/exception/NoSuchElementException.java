f1ad0bf04c69a857f0bdc364968e4ee94377c016
Karnely <Niceday2727@gmail.com>
2025-04-11 13:16:08 +0300

    11 спринт
    Финальное задание 11 спринта

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NoSuchElementException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class NoSuchElementException extends RuntimeException {
+    public NoSuchElementException(String message) {
+        super(message);
+    }
+}
