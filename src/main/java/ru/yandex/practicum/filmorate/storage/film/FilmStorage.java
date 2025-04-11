9ebb9393823a720a2d7807a1373bd15077455128
Karnely <Niceday2727@gmail.com>
2025-04-11 13:21:56 +0300

    11 спринт
    Финальное задание 11 спринта

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,8 +6,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     Optional<Film> getFilm(int id);
+
     List<Film> getAllFilms();
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,8 +6,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     User addUser(User user);
+
     User updateUser(User user);
+
     Optional<User> getUser(int id);
+
     List<User> getAllUsers();
 }
